56d56983fc5de2e3c2aa2a88b8eeae1d62a5805b
BrenoOPrado <breno.o.prado@hotmail.com>
2023-03-03 18:56:18 -0300

    Teste do report de product corrigido e função generate em simple_report criada :turtle:

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,29 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def generate(data):
+        manufacturing_date = []
+        expiration_date = []
+        products_by_company = {}
+        now_time = datetime.now().strftime("%Y-%m-%d")
+        for product in data:
+            manufacturing_date.append(product["data_de_fabricacao"])
+            if product["data_de_validade"] >= now_time:
+                expiration_date.append(product["data_de_validade"])
+            if product["nome_da_empresa"] in products_by_company:
+                products_by_company[product["nome_da_empresa"]] += 1
+            else:
+                products_by_company[product["nome_da_empresa"]] = 1
+        return (
+            "Data de fabricação mais antiga: "
+            f"{min(manufacturing_date)}\n"
+            "Data de validade mais próxima: "
+            f"{min(expiration_date)}\n"
+            "Empresa com mais produtos: "
+            f"{max(products_by_company, key= products_by_company.get)}"
+        )
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,16 +2,24 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    data = {
+    data = dict({
         "id": 3,
         "nome_do_produto": "sorvete",
         "nome_da_empresa": "trybe",
         "data_de_fabricacao": "2023-02-06",
         "data_de_validade": "2023-03-22",
         "numero_de_serie": "EX52 2034 8595 7904 7131",
-        "instrucoes_de_armazenamento": "guardar em lugar gelado"
-    }
-    result = Product.__repr__(data.items)
+        "instrucoes_de_armazenamento": "Guardar em lugar gelado"
+    })
+    result = Product(
+        data["id"],
+        data["nome_do_produto"],
+        data["nome_da_empresa"],
+        data["data_de_fabricacao"],
+        data["data_de_validade"],
+        data["numero_de_serie"],
+        data["instrucoes_de_armazenamento"],
+    )
     expect = (
         f"O produto {data['nome_do_produto']}"
         f" fabricado em {data['data_de_fabricacao']}"
@@ -19,5 +27,5 @@ def test_cria_produto():
         f" até {data['data_de_validade']}"
         f" precisa ser armazenado {data['instrucoes_de_armazenamento']}."
     )
-    assert expect in result
+    assert expect == str(result)
     # pass  # Seu teste deve ser escrito aqui
