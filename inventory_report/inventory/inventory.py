c7800225eff61fa352411ad12a8620ee5bb52ce7
BrenoOPrado <breno.o.prado@hotmail.com>
2023-03-04 12:07:02 -0300

    Função de gerar relatório por um arquivo json :turtle:

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,37 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    @staticmethod
-    def import_data(path, type):
+    @classmethod
+    def import_data(cls, path, type):
+        if '.csv' in path:
+            res = cls.import_data_csv(path, type)
+        elif '.json' in path:
+            res = cls.import_data_json(path, type)
+        return res
+
+    @classmethod
+    def import_data_csv(cls, path, type):
         with open(path) as file:
             csv_file = csv.DictReader(file)
             data = []
             for item in csv_file:
                 data.append(item)
-            if type == "simples":
-                res = SimpleReport.generate(data)
-            elif type == "completo":
-                res = CompleteReport.generate(data)
-            return res
+            return cls.generate_report(data, type)
+
+    @classmethod
+    def import_data_json(cls, path, type):
+        with open(path) as file:
+            data = json.load(file)
+            return cls.generate_report(data, type)
+
+    @staticmethod
+    def generate_report(data, type):
+        if type == "simples":
+            res = SimpleReport.generate(data)
+        elif type == "completo":
+            res = CompleteReport.generate(data)
+        return res
