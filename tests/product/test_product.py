a736aba8f5843d85820bc8d3dcd52111c266a326
BrenoOPrado <breno.o.prado@hotmail.com>
2023-03-06 14:42:14 -0300

    Teste de product_report feito e test de produc corrigido :turtle:

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -11,7 +11,7 @@ def test_cria_produto():
         "numero_de_serie": "EX52 2034 8595 7904 7131",
         "instrucoes_de_armazenamento": "Guardar em lugar gelado"
     })
-    result = Product(
+    result = str(Product(
         data["id"],
         data["nome_do_produto"],
         data["nome_da_empresa"],
@@ -19,13 +19,10 @@ def test_cria_produto():
         data["data_de_validade"],
         data["numero_de_serie"],
         data["instrucoes_de_armazenamento"],
-    )
-    expect = (
-        f"O produto {data['nome_do_produto']}"
-        f" fabricado em {data['data_de_fabricacao']}"
-        f" por {data['nome_da_empresa']} com validade"
-        f" até {data['data_de_validade']}"
-        f" precisa ser armazenado {data['instrucoes_de_armazenamento']}."
-    )
-    assert expect == str(result)
+    ))
+    assert data["nome_do_produto"] in result
+    assert data["data_de_fabricacao"] in result
+    assert data["nome_da_empresa"] in result
+    assert data["data_de_validade"] in result
+    assert data["instrucoes_de_armazenamento"] in result
     # pass  # Seu teste deve ser escrito aqui
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    data = dict({
+        "id": 3,
+        "nome_do_produto": "sorvete",
+        "nome_da_empresa": "trybe",
+        "data_de_fabricacao": "2023-02-06",
+        "data_de_validade": "2023-03-22",
+        "numero_de_serie": "EX52 2034 8595 7904 7131",
+        "instrucoes_de_armazenamento": "Guardar em lugar gelado"
+    })
+    result = str(Product(
+        data["id"],
+        data["nome_do_produto"],
+        data["nome_da_empresa"],
+        data["data_de_fabricacao"],
+        data["data_de_validade"],
+        data["numero_de_serie"],
+        data["instrucoes_de_armazenamento"],
+    ))
+    expect = (
+        f"O produto {data['nome_do_produto']}"
+        f" fabricado em {data['data_de_fabricacao']}"
+        f" por {data['nome_da_empresa']} com validade"
+        f" até {data['data_de_validade']}"
+        f" precisa ser armazenado {data['instrucoes_de_armazenamento']}."
+    )
+    assert expect == result
+    # pass  # Seu teste deve ser escrito aqui
